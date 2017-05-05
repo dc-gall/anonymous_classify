87c728c0407657a4fd3769133428a3349945ed83
dcinside <dd@hoto.us>
2017-05-05 20:10:21 +0900

    Update anonymous_classify.addon.php

--- anonymous_classify.addon.php
+++ anonymous_classify.addon.php
@@ -1,6 +1,4 @@
 <?php
-    $first = array("노래하는", "책읽는", "익명의", "구르는", "기여하는", "위키하는", "앉아있는", "날아가는", "놀고있는", "잉여한", "글쓰는", "뛰고있는", "자고있는", "살아있는", "학생", "편집하는", "놀러가는", "수영하는", "게임하는", "손짓하는", "구경하는", "눈팅중인");
-    $second = array("리브라", "리디버그", "리브렌", "위키러", "위키니트", "위키냥", "리버티", "리버덕", "리돌이", "륙", "에르빌", "리브레비", "리브리", "리브봇", "프라하", "리브맨", "반달가위", "리브크레", "세피로트", "무냐", "시조새", "드루아", "리브레키", "리브룬", "리브레드", "리프리", "레브", "니브레쿤", "리트랙터", "어그롭스");
     if(!defined("__XE__")) exit();
 
 	if($called_position == 'after_module_proc' && $this->act == "procBoardInsertDocument") {
@@ -13,17 +11,9 @@
 
 				$args->document_srl = $this->get('document_srl');
 				if(isset($logged_info)) {
-					$first_rand = hexdec(substr(md5($args->document_srl.$addon_info->security_code.$logged_info->member_srl),0,6)) % count($first);
-					$second_rand = hexdec(substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6)) % count($second);
-					
-					$args->nick_name = $first[$first_rand]. " " . $second[$second_rand];
-					//$args->nick_name = "리브러_".substr(md5($args->document_srl.$addon_info->security_code.$logged_info->member_srl),0,6);
+					$args->nick_name = "익명_" . (hexdec(substr(md5($args->document_srl.$addon_info->security_code.$logged_info->email_address),0,6)) % 100);
 				} else {
-					$first_rand = hexdec(substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6)) % count($first);
-					$second_rand = (hexdec(substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6)) * 3) % count($second);
-					
-					$args->nick_name = $first[$first_rand]. " " . $second[$second_rand];
-					//$args->nick_name = "리브러_".substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6);
+					$args->nick_name = "익명_" . (hexdec(substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6)) % 100);
 				}
 				executeQuery('addons.anonymous_classify.updatedocuments', $args);
 		}
@@ -48,21 +38,11 @@
 					$check = $oget_member_srl == $logged_info->member_srl ? false : true;
 				}
 
-				if($check) {
-					if(isset($logged_info)) {
-						$first_rand = hexdec(substr(md5($args->document_srl.$addon_info->security_code.$logged_info->member_srl),0,6)) % count($first);
-						$second_rand = hexdec(substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6)) % count($second);
-
-						$args->nick_name = $first[$first_rand]. " " . $second[$second_rand] . "_" . (hexdec(substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6)) % 100);
-						#$args->nick_name = "리브러_".substr(md5($args->document_srl.$addon_info->security_code.$logged_info->member_srl),0,6);
-					} else {
-						$first_rand = hexdec(substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6)) % count($first);
-						$second_rand = (hexdec(substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6)) * 3) % count($second);
-						
-						$args->nick_name = $first[$first_rand]. " " . $second[$second_rand] . "_" . (hexdec(substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6)) % 100);
-						#$args->nick_name = "리브러_".substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6);
-					}
-				} else {$args->nick_name = "익명의 리브러"; }
+				if(isset($logged_info)) {
+					$args->nick_name = "익명_" . (hexdec(substr(md5($args->document_srl.$addon_info->security_code.$logged_info->email_address),0,6)) % 100);
+				} else {
+					$args->nick_name = "익명_" . (hexdec(substr(md5($args->document_srl.$addon_info->security_code.$_SERVER['REMOTE_ADDR']),0,6)) % 100);
+				}
 				executeQuery('addons.anonymous_classify.updatecomments', $args);
 		}
 	}
